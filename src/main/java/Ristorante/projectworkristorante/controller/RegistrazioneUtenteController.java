ef94bf98b0e711e31ffa29b800113f1e282edf90
BartolomeoCenisio <bartolo.cenisio@gmail.com>
2024-03-11 17:51:52 +0100

    creazione controller

---
+++ src/main/java/Ristorante/projectworkristorante/controller/RegistrazioneUtenteController.java
@@ -0,0 +1,9 @@
+package Ristorante.projectworkristorante.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registrazioneutente")
+public class RegistrazioneUtenteController {
+}
