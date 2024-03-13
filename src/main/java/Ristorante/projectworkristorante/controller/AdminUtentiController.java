3fb1ee1ca2889a86322ffa093e93ff5e511bcdbb
Alizetta <alice.picone17@gmail.com>
2024-03-13 10:51:51 +0100

    implementato in adminutenti la visualizzazione di tutti gli utenti registrati

--- src/main/java/Ristorante/projectworkristorante/controller/AdminUtentiController.java
+++ src/main/java/Ristorante/projectworkristorante/controller/AdminUtentiController.java
@@ -1,14 +1,22 @@
 package Ristorante.projectworkristorante.controller;
 
+import Ristorante.projectworkristorante.model.Utente;
+import Ristorante.projectworkristorante.service.UtenteService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/adminutenti")
 public class AdminUtentiController {
+
+    @Autowired
+    private UtenteService utenteService;
 
     @GetMapping
     public String getPage(HttpSession session,
@@ -16,6 +24,9 @@ public class AdminUtentiController {
 
         if(session.getAttribute("admin") == null)
             return  "redirect:/loginadmin";
+
+        List<Utente> utenti = utenteService.getUtenti();
+        model.addAttribute("utenti", utenti);
 
         return "adminutenti";
     }
